91677dc3343166c184c605f290eb15c3776b9fa9
ricardoszanata <ricardoszanata@gmail.com>
2026-04-09 08:02:52 -0300

    Acerto do login para enviar os dados via URLSegment

--- uacesso.pas
+++ uacesso.pas
@@ -4,12 +4,16 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.Edit;
 
 type
   Tfrmacesso = class(TForm)
+
   private
     { Private declarations }
+
+    procedure teste();
   public
     { Public declarations }
   end;
@@ -20,5 +24,11 @@ var
 implementation
 
 {$R *.fmx}
+
+{ Tfrmacesso }
+
+procedure Tfrmacesso.teste;
+begin
+end;
 
 end.
